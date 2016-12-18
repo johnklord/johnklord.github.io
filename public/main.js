7db612ff3771c44834b759c36861f05b041aa9d0
Xun <xunchen369@gmail.com>
2016-12-18 00:43:01 -0800

    working

---
+++ public/main.js
@@ -0,0 +1,18 @@
+(function(){
+  $(document).ready(function(){
+    (function addEmailLink(){
+      var email = $("#email");
+      var emailAddr = email.text().trim();
+      var subject= "Hey Kyle!";
+      var body = "I saw your resume! I really liked it!";
+      email.wrapInner('<a href="mailto:'+emailAddr+'?subject='+encodeURIComponent(subject)+'&body='+encodeURIComponent(body)+'">');
+    })();
+    (function addGoogleMapLink(){
+      var addressEle = $("#address");
+      var address = addressEle.text().trim();
+      var link = "http://maps.google.com/?q="+address;
+      addressEle.wrapInner('<a target="_blank" href="'+link+'">');
+    })();
+  })
+})();
+
