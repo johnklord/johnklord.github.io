bfaba15a8b695ea23d5480529af007ad3e18b841
Xun <xunchen369@gmail.com>
2016-12-19 22:12:31 -0800

    changes

--- home/resume/resume.js
+++ home/resume/resume.js
@@ -1,5 +1,10 @@
 (function(){
   $(document).ready(function(){
+    var pageSizeSelectionWrapper = $("#page-size-selection-wrapper");
+    var selectPageSizeBtn = $("#select-page-size-btn");
+    var downloadBtn =  $("#download-btn");
+
+
     (function addEmailLink(){
       var email = $("#email");
       var emailAddr = email.text().trim();
@@ -16,6 +21,16 @@
     setTimeout(function(){
       document.body.dispatchEvent(new Event('view-ready'));
     },2000);
+    downloadBtn.click(function(e){
+      pageSizeSelectionWrapper.toggleClass("invisible");
+      downloadBtn.toggleClass("hidden");
+      selectPageSizeBtn.toggleClass("hidden");
+    })
+    selectPageSizeBtn.click(function(e){
+      pageSizeSelectionWrapper.toggleClass("invisible");
+      downloadBtn.toggleClass("hidden");
+      selectPageSizeBtn.toggleClass("hidden");
+    })
   })
 })();
 
