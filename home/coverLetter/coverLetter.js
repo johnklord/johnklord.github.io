6a2cc153cc9ca34ef49d6c6b3e783cdcf2f59eb5
Xun <xunchen369@gmail.com>
2017-01-20 03:11:18 -0800

    implmented markdown

--- home/coverLetter/coverLetter.js
+++ home/coverLetter/coverLetter.js
@@ -1,23 +1,39 @@
 (function(){
   $(document).ready(function(){
-    var pageSizeSelectionWrapper = $("#page-size-selection-wrapper");
-    var selectPageSizeBtn = $("#select-page-size-btn");
-    var downloadBtn =  $("#download-btn");
 
-    setTimeout(function(){
-      document.body.dispatchEvent(new Event('view-ready'));
-    },2000);
-
-    downloadBtn.click(function(e){
-      pageSizeSelectionWrapper.toggleClass("invisible");
-      downloadBtn.toggleClass("hidden");
-      selectPageSizeBtn.toggleClass("hidden");
-    })
-    selectPageSizeBtn.click(function(e){
-      pageSizeSelectionWrapper.toggleClass("invisible");
-      downloadBtn.toggleClass("hidden");
-      selectPageSizeBtn.toggleClass("hidden");
-    })
+    $("#coverLetter").load("../coverLetter.template.html",null,function(){
+      $("#download-btn").click(function(e){
+        console.log('cliekd');
+        $("#page-size-selection-wrapper").toggleClass("invisible");
+        $("#download-btn").toggleClass("hidden");
+        $("#select-page-size-btn").toggleClass("hidden");
+      })
+      $("#select-page-size-btn").click(function(e){
+        $("#page-size-selection-wrapper").toggleClass("invisible");
+        $("#download-btn").toggleClass("hidden");
+        $("#select-page-size-btn").toggleClass("hidden");
+      })
+      var companyName = window.location.pathname.slice(0,-1).split("/").pop();
+      console.log(companyName);
+      $("#a4-form").click(function(){
+        window.location.href = '/pdfs/coverLetters/'+companyName+"/kyle_wang_"+companyName+"_coverLetter_a4.pdf";
+      })
+      $("#letter-form").click(function(){
+        window.location.href = '/pdfs/coverLetters/'+companyName+"/kyle_wang_"+companyName+"_coverLetter_letter.pdf";
+      })
+      setTimeout(function(){
+        document.body.dispatchEvent(new Event('view-ready'));
+      },2000);
+      $.ajax({
+        url: "./coverLetter.md", 
+        type: 'get', 
+        dataType: 'html',
+        async: true,
+        success: function(data) {
+          $("#markdown-content").html(marked(data));
+        } 
+      });
+    });
   })
 })();
 
